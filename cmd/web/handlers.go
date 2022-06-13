0d1a2870c93352f61d10f1dbeba82fbfddcc5158
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-06-13 23:13:14 +0900

    add handlers test

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -158,3 +158,7 @@ func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
